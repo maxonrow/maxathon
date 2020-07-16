8c9477ad44ad4545f7237e508ecd603c7c469bfd
Phua <gingsheng.phua@actechmy.com>
2020-07-16 10:29:18 +0800

    add ft & nft frame setup

---
+++ ft-sample/src/ft/creator.ts
@@ -0,0 +1,16 @@
+import { mxw, auth, utils } from 'mxw-sdk-js/dist';
+import { sha256, toUtf8Bytes } from 'mxw-sdk-js/dist/utils'
+import { sortObject } from 'mxw-sdk-js/dist/utils/misc';
+
+export default class Creator {
+    public wallet: mxw.Wallet;
+    public constructor(wallet: mxw.Wallet) {
+        this.wallet = wallet;
+    }
+
+    public xxx() {
+
+    }
+
+}
+
---
+++ nft-sample/src/main.ts
@@ -0,0 +1,58 @@
+import { nodeProvider } from './node/node';
+import { mxw } from 'mxw-sdk-js/dist/index';
+import KycData from './nft/creator';
+
+let silentRpc = nodeProvider.trace.silentRpc;
+let providerConnection: mxw.providers.Provider;
+let provider: mxw.Wallet;
+let issuer: mxw.Wallet;
+let middleware: mxw.Wallet;
+
+/**
+  * initialization
+  */
+providerConnection = new mxw.providers.JsonRpcProvider(
+  nodeProvider.connection,
+  nodeProvider
+)
+  .on('rpc', function (args) {
+    if ('response' == args.action) {
+      if (silentRpc) {
+        console.log('request', JSON.stringify(args.request));
+        console.log('response', JSON.stringify(args.response));
+      }
+    }
+  })
+  .on('responseLog', function (args) {
+    if (silentRpc) {
+      console.log(
+        'responseLog',
+        JSON.stringify({ info: args.info, response: args.response })
+      );
+    }
+  });
+
+provider = mxw.Wallet.fromMnemonic(nodeProvider.kyc.provider).connect(providerConnection);
+
+issuer = mxw.Wallet.fromMnemonic(nodeProvider.kyc.issuer);
+
+middleware = mxw.Wallet.fromMnemonic(nodeProvider.kyc.middleware).connect(providerConnection);
+
+let wallet = mxw.Wallet.createRandom().connect(providerConnection);
+
+
+
+const run = async () => {
+  
+  console.log('Wallet Address', '-', wallet.address);
+  console.log('Wallet Mnemonic', '-', wallet.mnemonic);
+
+  let result: String = "Success !!!";
+
+  console.log('\x1b[33m%s\x1b[0m', result);  //yellow
+
+  process.exit();
+
+}
+
+run();
